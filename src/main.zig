452514597a0bc68cae556387155c4d2206b2dd9f
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2025-05-10 02:23:01 +0300

    add extended example with multiple events

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,32 @@ const print = std.debug.print;
 
 const lib = @import("fibers_proto");
 
-fn doSomeWork(finished: *lib.Event) void {
-    defer finished.set();
+fn spawnRunnable(gpa: std.mem.Allocator, runnable: anytype) void {
+    const T = @TypeOf(runnable);
 
-    for (0..10) |k| {
-        print("{}\n", .{k});
-        std.Thread.sleep(100 * std.time.ns_per_ms);
-    }
+    const Wrapper = struct {
+        const Self = @This();
+
+        data: T,
+        gpa: std.mem.Allocator,
+
+        fn run(self: *Self) void {
+            self.data.run();
+        }
+
+        fn deinit(self: *Self) void {
+            const alloc = self.gpa;
+            alloc.destroy(self);
+        }
+    };
+
+    const wrp = gpa.create(Wrapper) catch @panic("oom");
+    wrp.* = .{
+        .data = runnable,
+        .gpa = gpa,
+    };
+
+    lib.spawnTask(Wrapper, Wrapper.run, wrp, Wrapper.deinit);
 }
 
 pub fn main() !void {
@@ -19,17 +38,56 @@ pub fn main() !void {
     }
 
     print("initializing...\n", .{});
-
     lib.initFiberEngine(gpa.allocator(), 4);
+
     defer {
         print("terminating...\n", .{});
         lib.termFiberEngine();
         print("done.\n", .{});
     }
 
-    var finished = lib.Event{};
+    const Worker = struct {
+        id: usize,
+        finished: *lib.Event,
 
-    lib.spawnTask(lib.Event, doSomeWork, &finished, null);
+        fn run(self: *@This()) void {
+            defer self.finished.set();
+
+            for (0..10) |k| {
+                print("{} - {}\n", .{ self.id, k });
+                // TBD: we can implement `timeout` to sleep without cpu usage
+                std.Thread.sleep(100 * std.time.ns_per_ms);
+            }
+        }
+    };
+
+    var events: [16]lib.Event = undefined;
+    for (&events, 0..) |*ev, k| {
+        ev.* = .{};
+
+        spawnRunnable(gpa.allocator(), Worker{
+            .id = k,
+            .finished = ev,
+        });
+    }
+
+    const WaitAll = struct {
+        events: []lib.Event,
+        finished: *lib.Event,
+
+        fn run(self: *@This()) void {
+            for (self.events) |*ev| {
+                ev.wait();
+            }
+            self.finished.set();
+        }
+    };
+
+    var finished = lib.Event{};
+    spawnRunnable(gpa.allocator(), WaitAll{
+        .events = &events,
+        .finished = &finished,
+    });
 
     finished.wait();
 }
