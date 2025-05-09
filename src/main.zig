0824a5f1a509bcfd85ee7385f1717c3b86854f5e
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2025-05-09 19:11:35 +0300

    spawn worker threads
    add `initFiberEngine` and `termFiberEngine` that spawns and terminates worker threads

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,11 @@
 //! you are building an executable. If you are making a library, the convention
 //! is to delete this file and start with root.zig instead.
 
+const std = @import("std");
+
+/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
+const lib = @import("fibers_proto");
+
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
@@ -13,34 +18,13 @@ pub fn main() !void {
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("Initializing fiber engine...\n", .{});
+    lib.initFiberEngine(4);
+    try stdout.print("Initialized!\n", .{});
+
+    try stdout.print("Terminating fiber engine...\n", .{});
+    lib.termFiberEngine();
+    try stdout.print("Done.\n", .{});
 
     try bw.flush(); // Don't forget to flush!
 }
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("fibers_proto_lib");
--- src/root.zig
+++ src/root.zig
@@ -4,10 +4,96 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
+const threading = @import("win32").system.threading;
+
+const Handle = *anyopaque;
+const DEFAULT_STACK_SIZE: usize = 8 * 1024;
+
+const SharedData = struct {
+    numThreads: u32 = 0,
+    started: u32 = 0,
+    shutdown: bool = false,
+    finished: u32 = 0,
+
+    mux: std.Thread.Mutex = .{},
+    cond: std.Thread.Condition = .{},
+
+    pub inline fn lock(self: *SharedData) void {
+        self.mux.lock();
+    }
+
+    pub inline fn unlock(self: *SharedData) void {
+        self.mux.unlock();
+    }
+    pub inline fn wait(self: *SharedData) void {
+        self.cond.wait(&self.mux);
+    }
+};
+
+var shared: SharedData = .{};
+
+pub fn initFiberEngine(numThreads: u32) void {
+    // convert our own thread to a fiber so it's part of the system
+    const mainFiber = threading.ConvertThreadToFiber(null) orelse @panic("failed to convert current thread to a fiber");
+    _ = mainFiber;
+
+    std.debug.assert(shared.numThreads == 0);
+    shared.numThreads = numThreads;
+
+    for (0..numThreads) |_| {
+        const handle = threading.CreateThread(
+            null,
+            DEFAULT_STACK_SIZE,
+            &workerThread,
+            null,
+            threading.STACK_SIZE_PARAM_IS_A_RESERVATION,
+            null,
+        ) orelse @panic("failed to create thread");
+
+        _ = handle;
+    }
+
+    // wait all threads to initialize
+    shared.lock();
+    defer shared.unlock();
+
+    while (shared.started != numThreads) {
+        shared.wait();
+    }
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+pub fn termFiberEngine() void {
+    shared.lock();
+    defer shared.unlock();
+
+    shared.shutdown = true;
+
+    while (shared.finished != shared.numThreads) {
+        shared.wait();
+    }
+}
+
+fn workerThread(threadParam: ?*anyopaque) callconv(.winapi) u32 {
+    _ = threadParam;
+
+    const fiber = threading.ConvertThreadToFiber(null) orelse @panic("failed to convert worker thread to a fiber");
+    _ = fiber;
+
+    shared.lock();
+    defer shared.unlock();
+
+    shared.started += 1;
+    shared.cond.broadcast();
+
+    defer {
+        shared.finished += 1;
+        shared.cond.broadcast();
+    }
+
+    while (!shared.shutdown) {
+        // ...
+        shared.wait();
+    }
+
+    return 0;
 }
